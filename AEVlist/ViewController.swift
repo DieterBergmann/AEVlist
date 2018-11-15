713ff17e5055439e6768fc7a5bde1e48cea27a06
Dieter Bergmann <dieterbergmann@arcor.de>
2018-11-15 11:26:13 +0100

    Initial Commit

---
+++ AEVlist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AEVlist
+//
+//  Created by Dieter Bergmann on 15.11.18.
+//  Copyright © 2018 Dieter Bergmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
